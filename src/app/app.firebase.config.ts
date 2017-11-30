7064748f0e426a917284c24d287033bcc5712c62
Isosimppu <joonas.fellman@gmail.com>
2017-11-30 11:26:20 +0200

    Muutokset v1

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG =   var config = {
+    apiKey: "AIzaSyC_7JynVGA0MoyFHXieL3jrPblfgcajk1U",
+    authDomain: "laureatestapp.firebaseapp.com",
+    databaseURL: "https://laureatestapp.firebaseio.com",
+    projectId: "laureatestapp",
+    storageBucket: "laureatestapp.appspot.com",
+    messagingSenderId: "176903901525"
+  };
