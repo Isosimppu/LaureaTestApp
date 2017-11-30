02825583c4c1f34de8d9095085e96d035aad47aa
Isosimppu <joonas.fellman@gmail.com>
2017-11-30 23:01:46 +0200

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,12 @@ import { AngularFireAuthModule } from "angularfire2/auth";
 import { MyApp } from './app.component';
 import { FIREBASE_CONFIG } from './app.firebase.config';
 
-import { CalendarModule } from 'ionic3-calendar';
-
 @NgModule({
   declarations: [
     MyApp
   ],
   imports: [
     BrowserModule,
-    CalendarModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
     AngularFireAuthModule
--- src/pages/calendar/calendar.ts
+++ src/pages/calendar/calendar.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { CalendarModule } from 'ionic3-calendar';
 
 /**
  * Generated class for the CalendarPage page.
